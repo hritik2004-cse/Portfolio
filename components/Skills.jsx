64d6fb132cf509a47c51aecfa319edc7b0ac2227
hritik2004-cse <hs1349507@gmail.com>
2025-08-07 10:48:36 +0530

    some adjustments

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { RiReactjsFill, RiNextjsFill, RiHtml5Fill, RiCss3Fill, RiJavascriptFill, RiGithubFill, RiNodejsFill} from 'react-icons/ri'
+import { RiReactjsFill, RiNextjsFill, RiHtml5Fill, RiCss3Fill, RiJavascriptFill, RiGithubFill, RiNodejsFill, RiTa} from 'react-icons/ri';
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiPostman } from "react-icons/si";
 
 const Skills = () => {
     return (
